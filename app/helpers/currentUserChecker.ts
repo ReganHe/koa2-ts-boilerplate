93f1337d24dd5c181ca5cf7cc6555a8a84168e25
Mark He <heke@hujiang.com>
2018-04-17 17:44:35 +0800

    删除无用参数

--- app/helpers/currentUserChecker.ts
+++ app/helpers/currentUserChecker.ts
@@ -4,9 +4,6 @@ import { User } from '../models/user';
 
 async function currentUserChecker(action: Action, value?: any) {
   // perform queries based on token from request headers
-  // const token = action.request.headers["authorization"];
-  const testfiled = action.request.testfiled;
-
   const auth = action.request.headers['authorization'];
   const token = auth.slice(7);
   const decoded = jwt.verify(token, 'secret');
